258939ba535de3ef0d67b3224576a4b6110780d5
tekerene <tekerene9@gmail.com>
2020-09-15 21:26:43 +0100

    edited the list component

--- src/app/list-todo/list-todo.component.ts
+++ src/app/list-todo/list-todo.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-library.add(fas, far);
-
 @Component({
   selector: 'app-list-todo',
   templateUrl: './list-todo.component.html',
